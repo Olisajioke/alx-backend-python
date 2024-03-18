d01c3042a3e4d739e9252317a519e442f3fb7bb9
Olisajioke <chijioke914@gmail.com>
2024-03-18 13:48:48 +0000

    pycodestyle

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,4 +7,5 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     '''Measures the total execution time for `n` calls to ran'''
-    return await asyncio.gather(*(task_wait_random(max_delay) for _ in range(n)))
+    return await asyncio.gather(*(task_wait_random(max_delay)
+                                  for _ in range(n)))
