6bd129990f6832ceb4932803d6f8d49cb9649ec7
Olisajioke <chijioke914@gmail.com>
2024-03-18 14:37:54 +0100

    pycodestyle

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,6 +7,7 @@ from asyncio import run
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
+
 async def measure_time(n: int, max_delay: int) -> float:
     """Measure the average execution time for wait_n."""
     start_time = time.time()
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -4,6 +4,7 @@ import asyncio
 from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    '''Measures the total execution time for `n` calls to task_wait_random.'''
+    '''Measures the total execution time for `n` calls to ran'''
     return await asyncio.gather(*(task_wait_random(max_delay) for _ in range(n)))
