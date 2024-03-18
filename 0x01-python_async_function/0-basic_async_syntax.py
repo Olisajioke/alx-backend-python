402820f426b6e60922bb62c3fa1f5e059ecddd1a
Olisajioke <chijioke914@gmail.com>
2024-03-18 14:25:05 +0100

    task 0. 4

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import asyncio
+import random
+
+async def wait_random(max_delay: int = 10) -> float:
+    """Asynchronous coroutine that waits for a random delay."""
+    return random.uniform(0, max_delay)
---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module to measure the runtime of wait_n coroutine.
+"""
+import time
+from asyncio import run
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """Measure the average execution time for wait_n."""
+    start_time = time.time()
+    await wait_n(n, max_delay)
+    end_time = time.time()
+    return (end_time - start_time) / n
