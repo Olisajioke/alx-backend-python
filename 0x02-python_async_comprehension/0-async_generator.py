e23c1c1a9363aa491d02aea195be0164f0c7cfb3
Olisajioke <chijioke914@gmail.com>
2024-03-19 09:09:49 +0100

    task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,23 +1,12 @@
 #!/usr/bin/env python3
 '''Coroutine that generates random numbers asynchronously.'''
+from typing import AsyncGenerator
 import asyncio
 import random
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     '''Generate random numbers asynchronously.'''
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
-
-
-async def print_yielded_values():
-    '''Prints values yielded by async_generator.'''
-    result = []
-    async for i in async_generator():
-        result.append(i)
-    print(result)
-
-
-if __name__ == "__main__":
-    asyncio.run(print_yielded_values())
+        yield random.random() * 10
